e2c793a28ab6c324dbc8647c80dd123db5257df3
shabbyonline <82105792+shabbyonline@users.noreply.github.com>
2021-04-12 19:03:34 +0500

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,395 @@
+<?php
+//* Start the engine
+include_once( get_template_directory() . '/lib/init.php' );
+
+//* Setup Theme
+include_once( get_stylesheet_directory() . '/lib/theme-defaults.php' );
+
+//* Subpage Header Code
+require_once('subpage-header.php');
+
+//* Set Localization (do not remove)
+load_child_theme_textdomain( 'parallax', apply_filters( 'child_theme_textdomain', get_stylesheet_directory() . '/languages', 'parallax' ) );
+
+//* Add Image upload to WordPress Theme Customizer
+add_action( 'customize_register', 'parallax_customizer' );
+function parallax_customizer(){
+	require_once( get_stylesheet_directory() . '/lib/customize.php' );
+}
+
+//* Include Section Image CSS
+include_once( get_stylesheet_directory() . '/lib/output.php' );
+
+global $blogurl;
+$blogurl = get_stylesheet_directory_uri();
+
+//* Enqueue scripts and styles
+add_action( 'wp_enqueue_scripts', 'parallax_enqueue_scripts_styles' );
+function parallax_enqueue_scripts_styles() {
+	// Styles
+	wp_enqueue_style( 'dashicons' );
+	wp_enqueue_style( 'custom', get_stylesheet_directory_uri() . '/css/allstyles.css', array() );
+	//wp_enqueue_style( 'PTSans', get_stylesheet_directory_uri() . '/fonts/PTSans/PTSans.css', array() );
+	
+	// Example to load third party site
+	//wp_enqueue_style( 'googlefonts', '//fonts.googleapis.com/css?family=PT+Sans:400,700', array() );	
+	
+	// Example to load JS file
+	//wp_enqueue_script( 'owlcarousel', get_stylesheet_directory_uri() . '/js/owl-carousel/owl.carousel.min.js', array('jquery') );
+	
+}
+
+/* DELETEME * No longer recommended by Google and seems to cause issuse with latest WP on some sites. -GW 
+
+Removes Query Strings from scripts and styles
+function remove_script_version( $src ){
+  if ( strpos( $src, 'uploads/bb-plugin' ) !== false || strpos( $src, 'uploads/bb-theme' ) !== false ) {
+    return $src;
+  }
+  else {
+    $parts = explode( '?ver', $src );
+    return $parts[0];
+  }
+}
+add_filter( 'script_loader_src', 'remove_script_version', 15, 1 );
+add_filter( 'style_loader_src', 'remove_script_version', 15, 1 ); */
+
+
+//* Add HTML5 markup structure
+add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
+
+//* Add viewport meta tag for mobile browsers
+add_theme_support( 'genesis-responsive-viewport' );
+
+//* Reposition the primary navigation menu
+//remove_action( 'genesis_after_header', 'genesis_do_nav' );
+//add_action( 'genesis_header', 'genesis_do_nav', 12 );
+
+// Add Search to Primary Nav
+//add_filter( 'genesis_header', 'genesis_search_primary_nav_menu', 10 );
+function genesis_search_primary_nav_menu( $menu ){
+    locate_template( array( 'searchform-header.php' ), true );
+}
+
+//* Add support for structural wraps
+add_theme_support( 'genesis-structural-wraps', array(
+	'header',
+	'nav',
+	'subnav',
+	'breadcrumb',
+	'footer-widgets',
+	'footer',
+) );
+
+// Add Read More Link to Excerpts
+add_filter('excerpt_more', 'get_read_more_link');
+add_filter( 'the_content_more_link', 'get_read_more_link' );
+function get_read_more_link() {
+   return '...&nbsp;<a class="readmore" href="' . get_permalink() . '">Read&nbsp;More &raquo;</a>';
+}
+
+//* Add support for 4-column footer widgets
+add_theme_support( 'genesis-footer-widgets', 0 );
+
+//* Customize the entry meta in the entry header (requires HTML5 theme support)
+add_filter( 'genesis_post_info', 'sp_post_info_filter' );
+function sp_post_info_filter($post_info) {
+	$post_info = '[post_date] [post_comments] [post_edit]';
+	return $post_info;
+}
+
+//* Custom Breadcrumb Hook 
+function breadcrumb_hook() {
+	do_action('breadcrumb_hook');
+}
+
+//* Remove breadcrumbs and reposition them
+remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
+add_action( 'breadcrumb_hook', 'genesis_do_breadcrumbs', 12 );
+
+// Modify Breadcrumbs Args
+add_filter( 'genesis_breadcrumb_args', 'malcolm_breadcrumb_args' );
+function malcolm_breadcrumb_args( $args ) {
+	$args['prefix'] = '<div class="breadcrumbs"><div class="wrap">';
+	$args['suffix'] = '</div></div>';
+	$args['sep'] = ' <span class="bread-sep">></span> ';
+	$args['heirarchial_attachments'] = true;
+	$args['heirarchial_categories'] = true;
+	$args['display'] = true;
+	$args['labels']['prefix'] = '';
+    return $args;
+}
+
+// Blog Widgets
+genesis_register_sidebar( array(
+	'id'			=> 'blog-sidebar',
+	'name'			=> __( 'Blog Widgets', 'thrive' ),
+	'description'	=> __( 'This is latest news widget', 'thrive' ),
+) );
+
+// Add Header Links Widget to Header
+//add_action( 'genesis_before', 'header_widget', 1 );
+	function header_widget() {
+	if (is_active_sidebar( 'header-links' ) ) {
+ 	genesis_widget_area( 'header-links', array(
+		'before' => '<div class="header-links">',
+		'after'  => '</div>',
+	) );
+}}
+
+// Unregister unused sidebar
+//unregister_sidebar( 'header-right' );
+
+// Previous / Next Post Navigation Filter For Genesis Pagination
+add_filter( 'genesis_prev_link_text', 'gt_review_prev_link_text' );
+function gt_review_prev_link_text() {
+        $prevlink = '&laquo;';
+        return $prevlink;
+}
+add_filter( 'genesis_next_link_text', 'gt_review_next_link_text' );
+function gt_review_next_link_text() {
+        $nextlink = '&raquo;';
+        return $nextlink;
+}
+
+/* Subpage Header Backgrounds - Utilizes: Featured Images & Advanced Custom Fields Repeater Fields */
+
+// AFC Repeater Setup - NOTE: Set Image Return Value to ID
+// Row Field Name:
+/* Commenting the subpage header code - Enable the code if needed but this will require ACF plugin and repeater addon to be installed on a site. */
+$rows = '';
+$rows = get_field('subpage_header_backgrounds', 5);
+// Counts the rows and selects a random row
+$row_count = count($rows);
+$i = rand(0, $row_count - 1);
+// Set Image size to be returned
+$image_size = 'subpage-header';
+// Get Image ID from the random row
+$image_id = $rows[ $i ]['background_image'];
+// Use Image ID to get Image Array
+$image_array = wp_get_attachment_image_src($image_id, $image_size);
+// Set "Default BG" to first value of the Image Array. $image_array[0] = URL;
+$default_bg = $image_array[0]; 
+
+
+// Custom function for getting background images
+function custom_background_image($postID = "") {
+	// Variables
+	global $default_bg;
+	global $postID;
+	global $blog_slug;
+	
+	$currentID = get_the_ID();
+	$blogID = get_option( 'page_for_posts');
+	$parentID = wp_get_post_parent_id( $currentID );
+
+	// is_home detects if you're on the blog page- must be set in admin area
+	if( is_home() ) {
+		$currentID = $blogID;
+	} 
+	// Else if post page, set ID to BlogID.
+	elseif( is_home() || is_single() || is_archive() || is_search() ) {
+		$currentID = $blogID;
+	}
+
+	// Try to get custom background based on current page/post
+	$currentBackground = wp_get_attachment_image_src(get_post_thumbnail_id($currentID), 'subpage-header');
+	//Current page/post has no custom background loaded
+	if(!$currentBackground) {
+		// Find blog ID
+		$blog_page = get_page_by_path($blog_slug, OBJECT, 'page');
+		if ($blog_page) {
+			$blogID = $blogID;
+			$currentID = $blogID;
+		}
+		// Else if post page, set ID to BlogID.
+		elseif(is_single() || is_archive()) {
+			$currentID = $blogID; 
+		}
+
+		// Current page has a parent
+		if($parentID) {
+			// Try to get parents custom background
+			$parent_background = wp_get_attachment_image_src(get_post_thumbnail_id($parentID), 'subpage-header');
+			// Set parent background if it exists
+			if($parent_background) {
+				$background_image = $parent_background[0];
+			}
+			// Set default background
+			else {
+				$background_image = $default_bg;
+			}
+		}
+		// NO parent or no parent background: set default bg.
+		else {
+			$background_image = $default_bg;
+		}
+	}
+	// Current Page has a custom background: use that
+	else {
+		$background_image = $currentBackground[0];
+	}
+	return $background_image;
+}
+
+
+//* Reposition the primary navigation menu
+remove_action( 'genesis_after_header', 'genesis_do_nav' );
+add_action( 'genesis_after_header', 'genesis_do_nav', 12 );
+
+// Add Additional Image Sizes
+add_image_size( 'subpage-header', 1900, 162, true );
+add_image_size( 'news-thumb', 260, 150, false );
+add_image_size( 'news-full', 800, 300, false );
+add_image_size( 'mailchimp', 564, 9999, false );
+add_image_size( 'amp', 600, 9999, false  );
+
+
+// Gravity Forms confirmation anchor on all forms
+add_filter( 'gform_confirmation_anchor', '__return_true' );
+
+
+// Button and Link Shortcode
+// Usage: [button url="https://www.google.com"] Button Shortcode [/button]
+// Usage: [link url=”tel:1-817-447-9194″ onClick=”onClick=”ga(‘send’, ‘event’, { eventCategory: ‘Click to Call’, eventAction: ‘Clicked Phone Number’, eventLabel: ‘Header Number’});”]
+/* Enable the code if needed
+function button_shortcode($atts, $content = null) {
+  extract( shortcode_atts( array(
+	  'url' => '#',
+	  'target' => '_self',
+	  'onclick' => '',
+
+  ), $atts ) 
+);
+return '<a target="' . $target . '" href="' . $url . '" class="button" onClick="' . $onclick . '"><span>' . do_shortcode($content) . '</span></a>';
+}
+add_shortcode('button', 'button_shortcode');
+
+function link_shortcode($atts, $content = null) {
+  extract( shortcode_atts( array(
+	  'url' => '#',
+	  'target' => '_self',
+	  'onclick' => '',
+  ), $atts ) 
+);
+return '<a target="' . $target . '" href="' . $url . '" onClick="' . $onclick . '">' . do_shortcode($content) . '</a>';
+}
+add_shortcode('link', 'link_shortcode');
+*/
+
+//* Declare WooCommerce support
+add_action( 'after_setup_theme', 'woocommerce_support' );
+function woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
+
+// Run shortcodes in Text Widgets
+add_filter('widget_text', 'do_shortcode');
+
+
+//Removing unused Default Wordpress Emoji Script - Performance Enhancer
+function disable_emoji_dequeue_script() {
+    wp_dequeue_script( 'emoji' );
+}
+add_action( 'wp_print_scripts', 'disable_emoji_dequeue_script', 100 );
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 ); 
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+
+// Removes Emoji Scripts 
+add_action('init', 'remheadlink');
+function remheadlink() {
+	remove_action('wp_head', 'rsd_link');
+	remove_action('wp_head', 'wp_generator');
+	remove_action('wp_head', 'index_rel_link');
+	remove_action('wp_head', 'wlwmanifest_link');
+	remove_action('wp_head', 'feed_links', 2);
+	remove_action('wp_head', 'feed_links_extra', 3);
+	remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+	remove_action('wp_head', 'start_post_rel_link', 10, 0);
+	remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+	remove_action('wp_head', 'wp_shortlink_header', 10, 0);
+	remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+}
+
+// Add "nav-primary" class to Main Menu as this gets removed when we reposition the menu inside header/widget area
+add_filter( 'genesis_attr_nav-header', 'thrive_custom_nav_id' );
+function thrive_custom_nav_id( $attributes ) {
+ 	$attributes['class'] = 'nav-primary';
+ 	return $attributes;
+}
+
+//****** AMP Customizations ******/
+
+/*add_action('amp_init','amp_css', 11);
+function amp_css() { 
+	require_once('css/amp.php');
+}
+
+//* Add Featured Images to AMP content
+add_action( 'pre_amp_render_post', 'amp_add_custom_actions' );
+function amp_add_custom_actions() {
+    add_filter( 'the_content', 'amp_add_featured_image' );
+}
+
+function amp_add_featured_image( $content ) {
+    if ( has_post_thumbnail() ) {
+        // Just add the raw <img /> tag; our sanitizer will take care of it later.
+        $image = sprintf( '<p class="featured-image">%s</p>', get_the_post_thumbnail(get_the_ID(), 'amp') );
+        $content = $image . $content;
+    }
+    return $content;
+}
+
+// Add Fav Icon to AMP Pages
+add_action('amp_post_template_head','amp_favicon');
+function amp_favicon() { ?>
+	<link rel="icon" href="<?php echo get_site_icon_url(); ?>" />
+<?php } 
+
+// Add Banner below content of AMP Pages
+add_action('ampforwp_after_post_content','amp_custom_banner_extension_insert_banner');
+function amp_custom_banner_extension_insert_banner() { ?>
+	<div class="amp-custom-banner-after-post">
+		<h2>CUSTOM AMP BANNER TEXT HERE IF NEEDED</h2>
+		<a class="ampforwp-comment-button" href="/contact-us/">
+			CONTACT US
+		</a>
+	</div>
+<?php } */
+
+//Sets the number of revisions for all post types
+add_filter( 'wp_revisions_to_keep', 'revisions_count', 10, 2 );
+function revisions_count( $num, $post ) {
+	$num = 3;
+    return $num;
+}
+
+// Enable Featured Images in RSS Feed and apply Custom image size so it doesn't generate large images in emails
+function featuredtoRSS($content) {
+global $post;
+if ( has_post_thumbnail( $post->ID ) ){
+$content = '<div>' . get_the_post_thumbnail( $post->ID, 'mailchimp', array( 'style' => 'margin-bottom: 15px;' ) ) . '</div>' . $content;
+}
+return $content;
+}
+ 
+add_filter('the_excerpt_rss', 'featuredtoRSS');
+add_filter('the_content_feed', 'featuredtoRSS');
+
+/* 
+ * Dequeue Gutenberg-hooked CSS file `wp-block-library.css` file from `wp_head()`
+ *
+ * @author Thrive Agency
+ * @since  12182018
+ * @uses   wp_dequeue_style
+ */
+add_action( 'wp_enqueue_scripts', function() {
+  wp_dequeue_style( 'wp-block-library' );
+});
+
+/* Display Featured Image on top of the post */
+add_action( 'genesis_entry_content', 'featured_post_image', 8 );
+function featured_post_image() {
+    if ( !is_singular('post'))  return;
+    the_post_thumbnail('full'); /*you can use medium, large or a custom size */
+}
