69c139976587c108f62cd77f090d343129bb89a7
Shabbir Aftab <shabby@thrivenetmarketing.com>
2021-05-12 18:58:28 +0500

    wp_enqueue_scripts

--- functions.php
+++ functions.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:70c22a198d178694b997e71f5a7749f07909f0ee216aba00aac7c81314154b7a
-size 13879
+oid sha256:fbc455fb2b201ef2d8247b8f40bf8857ec64ef9a15314bdd7e0d7da5219c3399
+size 13512
